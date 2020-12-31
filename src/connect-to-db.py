0f3fb87b5fc04d564eeb2f5b8a5878ddb39f35f7
Brandon Kramer <brandonleekramer@gmail.com>
2020-12-31 11:26:30 -0500

    adding alt_con_to_db for python

--- src/connect-to-db.py
+++ src/connect-to-db.py
@@ -8,10 +8,20 @@ connection = pg.connect(host = 'postgis1', database = 'sdad',
                         user = 'login', password = 'pwd')
 
 postgreSQL_select_Query = '''
-SELECT *  
+SELECT * 
 FROM schema_name.table_name 
 LIMIT 100
 '''
 
 df = pd.read_sql_query(postgreSQL_select_Query, con=connection)
 df 
+
+
+# alternative connection process using sqlalchemy
+import psycopg2 
+from sqlalchemy import create_engine
+
+db = create_engine('postgresql+psycopg2://login:pwd@postgis1/sdad')
+result_set = db.execute("SELECT * FROM schema_name.table_name LIMIT 100")
+for r in result_set:  
+    print(r)
